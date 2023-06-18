6c0a47b0aa73484a43c82199c38875cb9555ece0
𝖲𝖨𝖥 𝖥𝖠𝖧𝖨𝖬 <siffahim25@gmail.com>
2023-06-18 20:19:26 +0600

    handle cast error

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,18 +1,29 @@
+/* eslint-disable no-console */
 /* eslint-disable no-unused-expressions */
 import { ErrorRequestHandler } from 'express';
 import config from '../../config';
 import ApiError from '../../errors/customError';
+import handleCastError from '../../errors/handleCastError';
 import handleValidationError from '../../errors/handleValidationError';
 import { IGenericErrorMessage } from '../../interfaces/error';
 
 // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
 const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
+  config.env === 'development'
+    ? console.log('🤷‍♀️ GlobalErrorHandler', error)
+    : '';
+
   let statusCode = 400;
   let message = 'something went wrong';
   let errorMessages: IGenericErrorMessage[] = [];
 
   if (error?.name === 'ValidationError') {
     const beautifyError = handleValidationError(error);
+    statusCode = beautifyError.statusCode;
+    message = beautifyError.message;
+    errorMessages = beautifyError.errorMessages;
+  } else if (error?.name === 'CastError') {
+    const beautifyError = handleCastError(error);
     statusCode = beautifyError.statusCode;
     message = beautifyError.message;
     errorMessages = beautifyError.errorMessages;
